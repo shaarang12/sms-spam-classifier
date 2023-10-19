f177b1210034477f79eb3bb5e88499b8a67793d9
shaarang.pd <84028033+shaarang12@users.noreply.github.com>
2023-10-19 22:22:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import pickle
 import string
-from nltk.corpus import stopwords
 import nltk
+from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 
 ps = PorterStemmer()
